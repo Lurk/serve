736a2b0d56330e7a30905c5c51c64759be11347f
Serhiy Barhamon <serhiy.barhamon@check24.de>
2025-08-15 20:43:59 +0200

    extract tls code to a separate module

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,13 @@
 mod errors;
+mod tls;
 
-use axum::{
-    extract::Request,
-    http::{header::HOST, StatusCode, Uri},
-    response::Redirect,
-    Router,
-};
-use axum_server::tls_rustls::RustlsConfig;
+use axum::{http::StatusCode, Router};
 use clap::{Args, Parser, Subcommand};
 use clap_verbosity_flag::Verbosity;
-use notify::{
-    event::{DataChange, ModifyKind},
-    Config, Event, EventKind, RecommendedWatcher, RecursiveMode, Result as NotifyResult, Watcher,
-};
 use std::{
     net::{Ipv4Addr, SocketAddr},
     path::PathBuf,
-    time::Duration,
 };
-use tokio::{join, runtime::Handle, time::sleep};
 use tower_http::{
     compression::CompressionLayer,
     services::{ServeDir, ServeFile},
@@ -30,6 +19,8 @@ use tracing_appender::{
     non_blocking::WorkerGuard,
     rolling::{RollingFileAppender, Rotation},
 };
+
+use crate::tls::start_tls_server;
 
 #[derive(Args, Debug)]
 struct Tls {
@@ -134,75 +125,13 @@ async fn main() -> Result<(), errors::ServeError> {
         .into_make_service();
 
     match args.subcommand {
-        Some(Subcommands::Tls(tls)) => {
-            let config = RustlsConfig::from_pem_file(&tls.cert, &tls.key).await?;
-            tracing::info!("listening on {} with TLS", addr);
-
-            let (server, http_to_https_redirect, tls_watcher) = join!(
-                axum_server::bind_rustls(addr, config.clone()).serve(service),
-                init_http_to_https_redirect(tls.redirect_http, args.port, args.addr),
-                init_certificate_watch(config, &tls)
-            );
-            server?;
-            http_to_https_redirect?;
-            tls_watcher?;
-        }
+        Some(Subcommands::Tls(tls)) => start_tls_server(service, addr, tls).await?,
         None => {
             tracing::info!("listening on {}", addr);
             axum_server::bind(addr).serve(service).await?;
         }
     };
     Ok(())
-}
-
-async fn init_http_to_https_redirect(
-    should_redirect: bool,
-    port: u16,
-    addr: Ipv4Addr,
-) -> Result<(), errors::ServeError> {
-    if should_redirect && port == 443 {
-        tracing::info!("initializing redirect from HTTP to HTTPS");
-        let http_addr = SocketAddr::from((addr, 80));
-        let service = Router::new()
-            .fallback(redirect)
-            .layer(
-                TraceLayer::new_for_http()
-                    .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
-                    .on_response(DefaultOnResponse::new().level(Level::INFO)),
-            )
-            .into_make_service();
-        axum_server::bind(http_addr).serve(service).await?;
-    }
-
-    if should_redirect && port != 443 {
-        tracing::error!("HTTP to HTTPS redirect is enabled but HTTPS port is not 443");
-    }
-
-    Ok(())
-}
-
-async fn redirect(req: Request) -> Redirect {
-    let mut parts = req.uri().clone().into_parts();
-    parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
-
-    if parts.path_and_query.is_none() {
-        parts.path_and_query = Some("/".parse().unwrap());
-    }
-
-    let host = req
-        .headers()
-        .get(HOST)
-        .expect("HOST to be present in request");
-
-    let https_host = host
-        .to_str()
-        .expect("HOST to be a valid str")
-        .replace("80", "443");
-
-    parts.authority = Some(https_host.parse().expect("host to be valid"));
-
-    let destination = Uri::from_parts(parts).expect("Uri can be recustructed with HTTPS schema");
-    Redirect::permanent(destination.to_string().as_str())
 }
 
 fn init_logging(
@@ -242,57 +171,4 @@ fn init_logging(
         .init();
 
     Ok(None)
-}
-
-async fn init_certificate_watch(
-    tls_config: RustlsConfig,
-    serve_config: &Tls,
-) -> Result<(), errors::ServeError> {
-    let mut delay: u64 = 1;
-    let (tx, mut rx) = tokio::sync::mpsc::channel(1);
-    let rt = Handle::current();
-    let retry_tx = tx.clone();
-
-    let mut watcher = RecommendedWatcher::new(
-        move |res: NotifyResult<Event>| match res {
-            Ok(res) => {
-                if let EventKind::Modify(ModifyKind::Data(DataChange::Content)) = res.kind {
-                    let tx = tx.clone();
-                    rt.spawn(async move {
-                        tx.send(()).await.expect("to be able to send message");
-                    });
-                }
-            }
-            Err(e) => tracing::error!("watcher error: {}", e),
-        },
-        Config::default(),
-    )?;
-
-    watcher.watch(&serve_config.cert, RecursiveMode::NonRecursive)?;
-    watcher.watch(&serve_config.key, RecursiveMode::NonRecursive)?;
-
-    while rx.recv().await.is_some() {
-        tracing::info!("reloading rustls configuration");
-        match tls_config
-            .reload_from_pem_file(serve_config.cert.clone(), serve_config.key.clone())
-            .await
-        {
-            Ok(_) => {
-                tracing::info!("rustls configuration reload successiful");
-                delay = 1;
-            }
-            Err(e) => {
-                delay *= 2;
-                tracing::error!("rustls reload error: {}", e);
-                tracing::info!("sleep {} nanoseconds before retry", delay);
-                sleep(Duration::from_millis(delay)).await;
-                retry_tx
-                    .send(())
-                    .await
-                    .expect("to be able to send retry message");
-            }
-        };
-    }
-
-    Ok(())
 }
